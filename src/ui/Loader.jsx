1f174d5df75e62562a9d3d175461596d3abbd5fb
osama78assi <osama78assi@gmail.com>
2024-02-08 15:58:41 +0200

    build the project

--- src/ui/Loader.jsx
+++
@@ -1,9 +0,0 @@
-function Loader() {
-  return (
-    <div className="absolute inset-0 flex cursor-progress items-center justify-center bg-slate-200/20 backdrop-blur-sm">
-      <div className="loader"></div>
-    </div>
-  );
-}
-
-export default Loader;
